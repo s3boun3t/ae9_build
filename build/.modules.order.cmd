1071d7bc1daf62e0d849abe15e017f47fc5226b9
s3boun3t <lacsebastien@gmail.com>
2026-03-06 23:52:56 +0100

    cleanup: remove all build artifacts, stale headers and temp files

--- build/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo snd-hda-codec-ca0132.o; :; } > modules.order
--- build/.snd-hda-codec-ca0132.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_snd-hda-codec-ca0132.mod := printf '%s\n'   ca0132.o | awk '!x[$$0]++ { print("./"$$0) }' > snd-hda-codec-ca0132.mod
